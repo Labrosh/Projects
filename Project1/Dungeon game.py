4db8847ee0dd1668e1b1398be848f3cadcf6e6c8
Labrosh <vlabrosh@gmail.com>
2025-01-22 12:16:50 -0800

    Refactored and tested quit functions

--- Project1/Dungeon game.py
+++ Project1/Dungeon game.py
@@ -1,18 +1,35 @@
 import sys
 
-# A simple room description for now
-room_desc = "You find yourself inside a blank square room. There is an exit to the north."
 
-print("Welcome to the dungeon.\n")
-print("You should start by looking around, or maybe checking your inventory.\n")
+def room_desc():
+    room_desc = "You find yourself inside a blank square room. There is an exit to the north." # this will be the starting room description, but I want to work on making this change base on what is in the room
+    print(room_desc)
 
-while True:
+def intro():
+    print("Welcome to the dungeon.\n")
+    print("You should start by looking around, or maybe checking your inventory.\n")
+
+def player_action():
     player_action = input("What do you want to do? ").strip().lower()
-    
-    if player_action == "quit":
-        print("Goodbye, brave adventurer!")
-        sys.exit()
-    elif player_action == "look":
-        print(room_desc)
-    else:
-        print("I don't understand that word.")
+    return player_action
+
+def quit_game():
+    print("Goodbye, brave adventurer!")
+    sys.exit()
+
+def unknown_word():
+    print("I don't understand that word.")
+
+# now we will try and make the game logic
+def game_loop():
+    room_desc()
+    intro()
+    while True:
+        action = player_action()
+        if action == "quit":
+            quit_game()
+        else:
+            unknown_word()
+
+# I guess this will start the game?
+game_loop()
