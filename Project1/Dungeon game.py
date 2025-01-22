acdde458f5ce7fbc226ec323d972123edcefcc18
Labrosh <vlabrosh@gmail.com>
2025-01-22 12:36:41 -0800

    defined movement functions, cleaned up commands to use functions, tested to see if commands still worked (they did)

--- Project1/Dungeon game.py
+++ Project1/Dungeon game.py
@@ -1,5 +1,6 @@
 import sys
 
+player_location = "starting room"
 
 def room_desc():
     room_desc = "You find yourself inside a blank square room. There is an exit to the north." # this will be the starting room description, but I want to work on making this change base on what is in the room
@@ -20,16 +21,50 @@ def quit_game():
 def unknown_word():
     print("I don't understand that word.")
 
-# now we will try and make the game logic
+def move_north():
+    print("You move north.")
+    player_location = "north room"
+def move_south():
+    print("You move south.")
+    player_location = "south room"
+def move_east():
+    print("You move east.")
+    player_location = "east room"
+def move_west():
+    print("You move west.")
+    player_location = "west room"
+def inventory():
+    print("You have nothing in your inventory.")
+
+
+# lets make a commands dictionary - hopefully this works
+commands = {
+    "quit": quit_game,
+    "exit": quit_game,
+    "look": room_desc,
+    "inventory": inventory,
+    "north": move_north,
+    "south": move_south,
+    "east": move_east,
+    "west": move_west
+}
+
+
+# I think I want to keep game loop near the bottom so I can find it easier
 def game_loop():
     room_desc()
     intro()
     while True:
         action = player_action()
-        if action == "quit":
-            quit_game()
+        if action in commands:
+            command = commands[action]
+            if callable(command):
+                command()
+            else:
+                print(command)
         else:
             unknown_word()
+
 
 # I guess this will start the game?
 game_loop()
