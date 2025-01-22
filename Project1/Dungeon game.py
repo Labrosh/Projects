6cda11a37fcb74f3b1953b07e76d074b39e8d462
Labrosh <vlabrosh@gmail.com>
2025-01-22 12:49:21 -0800

    Added rooms, location logic, restricted movement, as well as debug for location

--- Project1/Dungeon game.py
+++ Project1/Dungeon game.py
@@ -1,10 +1,46 @@
 import sys
 
 player_location = "starting room"
+rooms = {
+    "starting room": {
+        "description": "You find yourself inside a blank square room. There is an exit to the north, south, east, and west.",
+        "exits": {
+            "north": "north room",
+            "south": "south room",
+            "east": "east room",
+            "west": "west room"
+        }
+    },
+    "north room": {
+        "description": "You are in a room with stone walls. There is an exit to the south.",
+        "exits": {
+            "south": "starting room"
+        }
+    },
+    "south room": {
+        "description": "You are in a damp, dark room. There is an exit to the north.",
+        "exits": {
+            "north": "starting room"
+        }
+    },
+    "east room": {
+        "description": "You are in a brightly lit room. There is an exit to the west.",
+        "exits": {
+            "west": "starting room"
+        }
+    },
+    "west room": {
+        "description": "You are in a room filled with ancient artifacts. There is an exit to the east.",
+        "exits": {
+            "east": "starting room"
+        }
+    }
+}
 
 def room_desc():
-    room_desc = "You find yourself inside a blank square room. There is an exit to the north." # this will be the starting room description, but I want to work on making this change base on what is in the room
-    print(room_desc)
+    global player_location
+    room = rooms[player_location]
+    print(room["description"])
 
 def intro():
     print("Welcome to the dungeon.\n")
@@ -21,21 +57,32 @@ def quit_game():
 def unknown_word():
     print("I don't understand that word.")
 
+def move(direction):
+    global player_location
+    current_room = rooms[player_location]
+    if direction in current_room["exits"]:
+        player_location = current_room["exits"][direction]
+        print(f"You move {direction}.")
+    else:
+        print("You can't go that way.")
+
 def move_north():
-    print("You move north.")
-    player_location = "north room"
+    move("north")
+
 def move_south():
-    print("You move south.")
-    player_location = "south room"
+    move("south")
+
 def move_east():
-    print("You move east.")
-    player_location = "east room"
+    move("east")
+
 def move_west():
-    print("You move west.")
-    player_location = "west room"
+    move("west")
+
 def inventory():
     print("You have nothing in your inventory.")
 
+def debug():
+    print(player_location)
 
 # lets make a commands dictionary - hopefully this works
 commands = {
@@ -46,7 +93,8 @@ commands = {
     "north": move_north,
     "south": move_south,
     "east": move_east,
-    "west": move_west
+    "west": move_west,
+    "debug": debug
 }
 
 
